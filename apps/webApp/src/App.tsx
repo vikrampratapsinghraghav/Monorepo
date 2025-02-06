d8ba558b0d1eb2745865282555653df1e94f83ff
vikram singh <vikramsingh@vikrams-MacBook-Air.local>
2025-02-06 10:54:52 +0400

    lang fix

--- apps/webApp/src/App.tsx
+++ apps/webApp/src/App.tsx
@@ -3,13 +3,13 @@ import InfiniteList from "./components/InfiniteList";
 import LanguageSwitcher from "./components/LanguageSwitcher";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 
-import { LanguageProvider, useLanguage } from '../../../packages/ui/i18n/LanguageContext';
+import { LanguageProvider } from '../../../packages/ui/i18n/LanguageContext';
 import { ThemeProvider } from '../../../packages/ui/utils/ThemeContext';
 
 
 function App() {
   return (
-    <div>
+  
     <ThemeProvider>
       <LanguageProvider>
         <ThemeSwitcher />
@@ -17,13 +17,9 @@ function App() {
         <InfiniteList />
       </LanguageProvider>
     </ThemeProvider>
-    </div>
+    
   );
-  return (
-    <div style={{ padding: "20px" }}>
-      <InfiniteList />
-    </div>
-  );
+ 
 }
 
 export default App;
